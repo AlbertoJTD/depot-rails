39d828def5a5898c94ec0b3ef694abc321de580e
AlbertoJTD <albertoJTD@outlook.com>
2022-01-05 13:16:49 -0600

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,16 @@
 class Product < ApplicationRecord
+    #Valitating fields
+    validates :title, :description, :image_url, :price, presence: true
+
+    #Validates that price is greater than or equal to 0.01
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+    #Validates that the title is unique
+    validates :title, uniqueness: true
+
+    #Validates the URL for images
+    validates :image_url, allow_blank: true, format: {
+        with: %r{\.(gif|jpg|png|jpeg)\z}i,
+        message: 'Must be a URL for GIF, JPG, JPEG or PNG image.'
+    }
 end
