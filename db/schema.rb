37f2ec011fe0723e859cf5b969872a7006db4cf9
AlbertoJTD <albertojtd@outlook.com>
2022-03-25 13:26:36 -0600

    Finding a cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_194925) do
+ActiveRecord::Schema.define(version: 2022_03_25_191444) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
