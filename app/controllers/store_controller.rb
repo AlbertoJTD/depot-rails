0146fb22c888803b4cf7e9526880e6bcb9c64efc
AlbertoJTD <albertojtd@outlook.com>
2022-03-25 18:47:13 -0600

    A counter has been added to the page.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title) #Display the products in alphabetical order
+    if session[:counter].nil?
+      session[:counter] = 0
+      puts "El valor es de: #{session[:counter]}"
+    else
+      session[:counter] += 1
+    end
   end
 end
