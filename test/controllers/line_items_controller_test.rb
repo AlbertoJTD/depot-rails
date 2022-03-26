ed433278f017516c61fafb32cc6039fbc8c8194d
AlbertoJTD <albertojtd@outlook.com>
2022-03-26 17:26:09 -0600

    Creating a smarter cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -46,14 +46,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to line_items_url
   end
 
-  test "should create line_item" do
+  test "should create line_item 2" do
     assert_difference('LineItem.count') do
       post line_items_url, params: { product_id: products(:ruby).id }
     end
 
     follow_redirect!
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
+    assert_select 'li', "1 \u00D7 Programming Ruby 1.9"
   end
     
 end
