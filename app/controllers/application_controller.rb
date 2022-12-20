1ff9599c42b60a3405fdfd96f4f9cd29ec40bc39
AlbertoJTD <albertojtd@outlook.com>
2022-12-19 18:54:30 -0600

    Internationalization: Selecting the Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale, :authorize
+  before_action :set_i18n_locale_from_params, :authorize
 
-  def set_locale
-    puts "Este es el lenguaje: #{params[:locale]}"
-    I18n.locale = params[:locale] || I18n.default_locale
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "#{params[:locale]} translations not available"
+        logger.error flash.now[:notice]
+      end
+    end
+    
+    # puts "Este es el lenguaje: #{params[:locale]}"
+    # I18n.locale = params[:locale] || I18n.default_locale
   end
 
   #around_action :set_locale
--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,16 @@ Rails.application.routes.draw do
   end
   get 'sessions/create'
   get 'sessions/destroy'
+
   resources :users
-  scope "(:locale)", defaults: {locale: 'en'}, locale: /en|es/  do
-    # scope "/:locale", defaults: {locale: 'en'}, constraints: {locale: /en | es/ } do
+  scope '(:locale)'  do
     resources :orders
     resources :line_items
     resources :carts
     root 'store#index', as: 'store_index'
     resources :products
-
     resources :products do
       get :who_bought, on: :member
     end
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
 end
