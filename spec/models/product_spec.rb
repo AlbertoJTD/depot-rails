164885812f889dfc0a62aa0df6c2a09ce9835e51
AlbertoJTD <albertojtd@outlook.com>
2025-08-02 17:50:30 -0600

    Add product tests using RSpec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,103 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  image_url   :string
+#  price       :decimal(8, 2)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'validations' do
+    context 'valid' do
+      it 'with title, description, image_url, and price' do
+        product = Product.new(title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg', price: 1)
+
+        expect(product).to be_valid
+      end
+
+      it 'with a price greather than or equal to 0.01' do
+        product = Product.new(title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg', price: 0.01)
+        expect(product).to be_valid
+
+        product.price = 1
+        expect(product).to be_valid
+
+        product.price = 10
+        expect(product).to be_valid
+      end
+
+      it 'with a valid image_url' do
+        %w[gif jpg png jpeg].each do |image_url|
+          product = Product.new(title: 'My Book Title', description: 'yyy', image_url: "image_#{rand(1000)}.#{image_url}", price: 1)
+          expect(product).to be_valid
+        end
+      end
+    end
+
+    context 'invalid' do
+      it 'without a title' do
+        product = Product.new(description: 'yyy', image_url: 'zzz.jpg', price: 1)
+
+        expect(product).to be_invalid
+        expect(product.errors[:title]).to include("can't be blank")
+      end
+
+      it 'without a description' do
+        product = Product.new(title: 'My Book Title', image_url: 'zzz.jpg', price: 1)
+
+        expect(product).to be_invalid
+        expect(product.errors[:description]).to include("can't be blank")
+      end
+
+      it 'without an image_url' do
+        product = Product.new(title: 'My Book Title', description: 'yyy', price: 1)
+
+        expect(product).to be_invalid
+        expect(product.errors[:image_url]).to include("can't be blank")
+      end
+
+      it 'without a price' do
+        product = Product.new(title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg')
+
+        expect(product).to be_invalid
+        expect(product.errors[:price]).to include("can't be blank")
+        expect(product.errors[:price]).to include('is not a number')
+      end
+
+      it 'with a price less than 0.01' do
+        product = Product.new(title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg', price: 0.00)
+        expect(product).to be_invalid
+
+        product.price = -1
+        expect(product).to be_invalid
+
+        product.price = -1000
+        expect(product).to be_invalid
+        expect(product.errors[:price]).to include('must be greater than or equal to 0.01')
+      end
+
+      it 'with a duplicated title' do
+        product = Product.new(title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg', price: 1)
+        product.save!
+
+        product2 = Product.new(title: 'My Book Title', description: 'yyy', image_url: 'zzz.jpg', price: 1)
+        expect(product2).to be_invalid
+        expect(product2.errors[:title]).to include('has already been taken')
+      end
+
+      it 'with a non-URL image_url' do
+        %w[not_a_url.foo not_a_url.bar xlsx pdf md].each do |image_url|
+          product = Product.new(title: 'My Book Title', description: 'yyy', image_url: "image_#{rand(1000)}.#{image_url}", price: 1)
+          expect(product).to be_invalid
+          expect(product.errors[:image_url]).to include('Must be a URL for GIF, JPG, JPEG or PNG image.')
+        end
+      end
+    end
+  end
+end
